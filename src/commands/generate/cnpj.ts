ee2fb319ea23330fe197040528aa5f9d4c1d08f2
Kaique Sacchi <sacchikaique@gmail.com>
2023-10-12 00:47:48 -0300

    feat: added generate.cnpj

--- src/commands/generate/cnpj.ts
+++ src/commands/generate/cnpj.ts
@@ -2,90 +2,93 @@ import { z } from "zod";
 import parseInput from "../../core-utils/parseInput";
 import { Command } from "commander";
 
-export default function registerGenerateCpf(program: Command) {
+export default function registerGenerateCnpj(program: Command) {
   program
-    .command("generate.cpf")
-    .option("-a --amount <amount>", "The amount of CPFs to generate", "1")
-    .option("-F --formatted", "Format the CPFs", false)
+    .command("generate.cnpj")
+    .option("-a --amount <amount>", "The amount of CNPJs to generate", "1")
+    .option("-F --formatted", "Format the CNPJs", false)
     .option("-S --sorted", "Sorts the list", false)
     .option(
       "-s --startsWith <startsWith>",
-      "Up to the first 9 digits of the CPFs",
+      "Up to the first 12 digits of the CNPJs",
       ""
     )
-    .action(cpf)
-    .description("Generates a list of CPFs");
+    .action(cnpj)
+    .description("Generates a list of CNPJs");
 }
 
 const InputSchema = z.object({
   amount: z.coerce
     .number({ invalid_type_error: "amount must be a number" })
-    .max(1000, "can only generate 1,000 CPFs at a time")
+    .max(1000, "can only generate 1,000 CNPJs at a time")
     .default(1),
   formatted: z.boolean().default(false),
   sorted: z.boolean().default(false),
   startsWith: z
     .string()
-    .max(9, "only the first 9 digits can be pre-defined")
+    .max(12, "only the first 12 digits can be pre-defined")
     .refine(
       (startsWith) =>
         ![
-          "000000000",
-          "111111111",
-          "222222222",
-          "333333333",
-          "444444444",
-          "555555555",
-          "666666666",
-          "777777777",
-          "888888888",
-          "999999999",
+          "000000000000",
+          "111111111111",
+          "222222222222",
+          "333333333333",
+          "444444444444",
+          "555555555555",
+          "666666666666",
+          "777777777777",
+          "888888888888",
+          "999999999999",
         ].includes(startsWith),
-      "CPFs with all same digits are invalid"
+      "CNPJs with all same digits are invalid"
     )
     .default(""),
 });
 
-function cpf(options: unknown) {
+function cnpj(options: unknown) {
   const parsedInput = parseInput(options, InputSchema);
 
-  let cpfs: string[] = [];
+  let CNPJs: string[] = [];
 
   for (let i = 0; i < parsedInput.amount; i++) {
     const startingDigitsAsString = parsedInput.startsWith.padEnd(
-      9,
+      12,
       `${Math.random()}`.slice(2)
     );
+
     const firstSecurityDigit = hashSecurityDigit(startingDigitsAsString);
     const secondSecurityDigit = hashSecurityDigit(
-      startingDigitsAsString.slice(1) + firstSecurityDigit
+      startingDigitsAsString + firstSecurityDigit
     );
-    cpfs.push(
+
+    CNPJs.push(
       `${startingDigitsAsString}${firstSecurityDigit}${secondSecurityDigit}`
     );
   }
 
   if (parsedInput.sorted) {
-    cpfs.sort();
+    CNPJs.sort();
   }
 
   if (parsedInput.formatted) {
-    cpfs = cpfs.map(
-      (cpf) =>
-        `${cpf.slice(0, 3)}.${cpf.slice(3, 6)}.${cpf.slice(6, 9)}-${cpf.slice(
-          9,
-          11
-        )}`
+    CNPJs = CNPJs.map(
+      (CNPJ) =>
+        `${CNPJ.slice(0, 2)}.${CNPJ.slice(2, 5)}.${CNPJ.slice(
+          5,
+          8
+        )}/${CNPJ.slice(8, 12)}-${CNPJ.slice(12)}`
     );
   }
 
-  console.log(cpfs.join("\n"));
+  console.log(CNPJs.join("\n"));
 }
 
+const weights = [2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6];
 function hashSecurityDigit(startingDigitsAsString: string) {
   let sum = 0;
   Array.from(startingDigitsAsString).forEach((digit, index) => {
-    sum += Number(digit) * (10 - index);
+    sum += Number(digit) * weights[startingDigitsAsString.length - 1 - index]!;
   });
   const mod = sum % 11;
   return `${mod < 2 ? "0" : 11 - mod}`;
--- src/core-utils/program.ts
+++ src/core-utils/program.ts
@@ -3,11 +3,13 @@ import { getVersion } from "./getVersion";
 
 import registerToast from "../commands/toast";
 import registerGenerateCpf from "../commands/generate/cpf";
+import registerGenerateCnpj from "../commands/generate/cnpj";
 
 const program = new Command("dev");
 
 program.version(getVersion());
 
+registerGenerateCnpj(program);
 registerGenerateCpf(program);
 registerToast(program);
 
