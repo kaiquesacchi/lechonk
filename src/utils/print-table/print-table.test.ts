5f1274cef5bdf8560c383dacc9f29d2dfffed9d4
Kaique Sacchi <sacchikaique@gmail.com>
2024-01-07 23:20:41 -0300

    feat(utils): added print-table

---
+++ src/utils/print-table/print-table.test.ts
@@ -0,0 +1,45 @@
+import printTable from "./print-table";
+
+describe("utils/print-table", () => {
+  const spyConsole = jest.spyOn(console, "log");
+
+  beforeEach(() => {
+    spyConsole.mockReset();
+  });
+
+  it("should print a minimal table", () => {
+    printTable({
+      header: ["a", "b", "c"],
+      body: [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+      ],
+      maxColumnLength: 5,
+    });
+
+    expect(spyConsole).toHaveBeenCalledTimes(3);
+    expect(spyConsole).toHaveBeenNthCalledWith(1, "a | b | c");
+    expect(spyConsole).toHaveBeenNthCalledWith(2, "---------");
+    expect(spyConsole).toHaveBeenNthCalledWith(3, "1 | 2 | 3\n4 | 5 | 6");
+  });
+
+  it("scale column length properly", () => {
+    printTable({
+      header: ["Big Header", "Small", "Tiny"],
+      body: [
+        ["abcdefghijklmnopqrstuvwxyz", "abcde", "abc"],
+        ["large cell", "abc", "def"],
+      ],
+      maxColumnLength: 5,
+    });
+
+    expect(spyConsole).toHaveBeenCalledTimes(3);
+    expect(spyConsole).toHaveBeenNthCalledWith(1, "Big … | Small | Tiny");
+    expect(spyConsole).toHaveBeenNthCalledWith(2, "--------------------");
+    expect(spyConsole).toHaveBeenNthCalledWith(
+      3,
+      // cspell:disable-next-line
+      "abcd… | abcde | abc \nlarg… | abc   | def "
+    );
+  });
+});
---
+++ src/utils/print-table/print-table.ts
@@ -0,0 +1,50 @@
+export default function printTable(input: {
+  header: readonly string[];
+  body: readonly string[][];
+  maxColumnLength: number;
+}) {
+  const columnLengths: number[] = [];
+  input.header.forEach((header, index) => {
+    columnLengths.push(
+      Math.min(
+        input.maxColumnLength,
+        Math.max(
+          header.length,
+          ...(input.body[index]?.map((row) => row.length) ?? [])
+        )
+      )
+    );
+  });
+
+  // Print header
+  console.log(
+    input.header
+      .map((cell, index) => formatCellWidth(cell, columnLengths[index]!))
+      .join(" | ")
+  );
+
+  // Print separator
+  console.log(
+    "".padEnd(
+      columnLengths.reduce((a, b) => a + b, 0) + 3 * (columnLengths.length - 1),
+      "-"
+    )
+  );
+
+  // Print body
+  console.log(
+    input.body
+      .map((row) =>
+        row
+          .map((cell, index) => formatCellWidth(cell, columnLengths[index]!))
+          .join(" | ")
+      )
+      .join("\n")
+  );
+}
+
+function formatCellWidth(cell: string, width: number) {
+  return cell.length > width
+    ? cell.slice(0, width - 1) + "…"
+    : cell.padEnd(width);
+}
